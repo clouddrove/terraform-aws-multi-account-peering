65770a084a607629cc69d400ae657392b4332780
Chetan Kesare <chetan.kesare@clouddrove.com>
2026-05-06 01:17:01 +0530

    fix: changed provider meta

--- versions.tf
+++ versions.tf
@@ -10,6 +10,6 @@ terraform {
   }
 
   provider_meta "aws" {
-    module_name = "clouddrove/terraform-aws-multi-account-peering"
+    user_agent = ["github.com/clouddrove/terraform-aws-multi-account-peering"]
   }
 }
